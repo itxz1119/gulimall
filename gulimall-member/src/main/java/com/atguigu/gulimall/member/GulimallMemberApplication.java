24baf3a69d984c5a0b01b64f8fc8377f22de0e82
Layne <Layne@qq.com>
2022-09-30 08:28:36 +0800

    项目初始结构

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -1,13 +1,23 @@
 package com.atguigu.gulimall.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/*
+* 想要远程调用别的服务 就要
+* 1.引入open-feign
+* 2.编写接口,决定调用哪一个服务
+* 3.加入@EnableFeignClients
+* */
+@MapperScan("com.atguigu.gulimall.member.dao")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign")
 @SpringBootApplication
 public class GulimallMemberApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GulimallMemberApplication.class, args);
     }
-
 }
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -3,11 +3,10 @@ package com.atguigu.gulimall.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GulimallProductApplication.class, args);
     }
-
 }
