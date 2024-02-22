3c3e2dc884aa4532448add24ea185f69ddc08115
Layne <Layne@qq.com>
2024-02-22 16:51:35 +0800

    测试git3

--- gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/oss/OssTest.java
+++ gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/oss/OssTest.java
@@ -17,7 +17,7 @@ public class OssTest {
 //        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
 //        String endpoint = "oss-cn-hangzhou.aliyuncs.com";
 //        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-//        String accessKeyId = "";222
+//        String accessKeyId = "";333
 //        String accessKeySecret = "";
 //        // 创建OSSClient实例。
 //        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
