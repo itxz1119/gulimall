3d681b0fb21eb9d7ac02d483647994005a8367a2
zhangxiaoyi <Layne@qq.com>
2024-04-18 09:21:48 +0800

    0418--提交文件6

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 import java.util.Map;
 
 /**
- * 让我们测试合并分支1111
+ * 让我们测试合并分支666
  */
 @Service("orderItemService")
 public class OrderItemServiceImpl extends ServiceImpl<OrderItemDao, OrderItemEntity> implements OrderItemService {
